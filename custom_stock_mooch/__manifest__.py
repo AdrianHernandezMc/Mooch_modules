295fbed5e7cfeab134e163f2323de26769325760
DevBarucTI <Desarrollo@mooch-mx.com>
2025-04-22 15:18:49 -0600

    [ADD] Field of view and update manifest

--- custom_stock_mooch/__manifest__.py
+++ custom_stock_mooch/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Custom Stock Move',
-    'version': '17.0.0.0.0',
+    'version': '17.0.0.1.0',
     'author': ['Baruc Alvarez'],
     'category': 'Inventory',
     'summary': 'Campos traslados para Mooch',
