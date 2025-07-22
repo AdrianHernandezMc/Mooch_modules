fcc80b5959f386a30c5f412a587abfc51b8b9e27
Adrian.Hernandez <soporteTI@mooch-mx.com>
2025-07-22 11:05:46 -0600

    [CHG]season values for calculate profit is no more aviable for select on product.

--- product_mooch/models/res_config_settings.py
+++ product_mooch/models/res_config_settings.py
@@ -10,17 +10,17 @@ class ResConfigSettings(models.TransientModel):
         help="Campo vacío para controlar saltos de línea en la vista"
     )
     # — Para Ropa —
-    sale_type_clothes_cash   = fields.Float(string='Moda (Contado %)',   help="Porcentaje contado para productos de moda")
+    sale_type_clothes_cash   = fields.Float(string='Moda (Contado %)', help="Porcentaje contado para productos de moda")
     sale_type_clothes_credit = fields.Float(string='Moda (Crédito %)', help="Porcentaje crédito para productos de moda")
     # — Para Hogar —
-    sale_type_home_cash      = fields.Float(string='Hogar (Contado %)',   help="Porcentaje contado para productos de hogar")
+    sale_type_home_cash      = fields.Float(string='Hogar (Contado %)',  help="Porcentaje contado para productos de hogar")
     sale_type_home_credit    = fields.Float(string='Hogar (Crédito %)',  help="Porcentaje crédito para productos de hogar")
     # — Para Calzado —
-    sale_type_shoe_cash      = fields.Float(string='Calzado (Contado %)',   help="Porcentaje contado para productos de calzado")
+    sale_type_shoe_cash      = fields.Float(string='Calzado (Contado %)', help="Porcentaje contado para productos de calzado")
     sale_type_shoe_credit    = fields.Float(string='Calzado (Crédito %)', help="Porcentaje crédito para productos de calzado")
     # — Para Temporada —
-    sale_type_season_cash    = fields.Float(string='Temporada (Contado %)',   help="Porcentaje contado para productos de temporada")
-    sale_type_season_credit  = fields.Float(string='Temporada (Crédito %)', help="Porcentaje crédito para productos de temporada")
+    # sale_type_season_cash    = fields.Float(string='Temporada (Contado %)', help="Porcentaje contado para productos de temporada")
+    # sale_type_season_credit  = fields.Float(string='Temporada (Crédito %)', help="Porcentaje crédito para productos de temporada")
 
     @api.model
     def set_values(self):
@@ -32,8 +32,8 @@ class ResConfigSettings(models.TransientModel):
         params.set_param('product_mooch.sale_type_home_credit',    self.sale_type_home_credit)
         params.set_param('product_mooch.sale_type_shoe_cash',      self.sale_type_shoe_cash)
         params.set_param('product_mooch.sale_type_shoe_credit',    self.sale_type_shoe_credit)
-        params.set_param('product_mooch.sale_type_season_cash',    self.sale_type_season_cash)
-        params.set_param('product_mooch.sale_type_season_credit',  self.sale_type_season_credit)
+        # params.set_param('product_mooch.sale_type_season_cash',    self.sale_type_season_cash)
+        # params.set_param('product_mooch.sale_type_season_credit',  self.sale_type_season_credit)
         # dispara tu cron para que recalcule precios en los productos afectados
         self.env['product.template'].cron_recompute_product_prices()
 
@@ -48,7 +48,7 @@ class ResConfigSettings(models.TransientModel):
             'sale_type_home_credit':    float(params.get_param('product_mooch.sale_type_home_credit',    default=0.0)),
             'sale_type_shoe_cash':      float(params.get_param('product_mooch.sale_type_shoe_cash',      default=0.0)),
             'sale_type_shoe_credit':    float(params.get_param('product_mooch.sale_type_shoe_credit',    default=0.0)),
-            'sale_type_season_cash':    float(params.get_param('product_mooch.sale_type_season_cash',    default=0.0)),
-            'sale_type_season_credit':  float(params.get_param('product_mooch.sale_type_season_credit',  default=0.0)),
+            # 'sale_type_season_cash':    float(params.get_param('product_mooch.sale_type_season_cash',    default=0.0)),
+            # 'sale_type_season_credit':  float(params.get_param('product_mooch.sale_type_season_credit',  default=0.0)),
         })
         return res
