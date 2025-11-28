cbed956708a81f6b38c84f66dc81a203283f346e
Adrian.Hernandez <soporteTI@mooch-mx.com>
2025-11-28 17:23:27 -0600

    [CHG]security has been modifed to allow validate order

--- custom_purchase_mooch/models/hr_department.py
+++ custom_purchase_mooch/models/hr_department.py
@@ -8,10 +8,9 @@ class Department(models.Model):
         string='Cuenta Analítica',
         help='Cuenta analítica vinculada a este departamento',
     )
-    
     budget_password = fields.Char(
         string='Contraseña de Presupuesto',
-        help='Contraseña para acceder al presupuesto de este departamento',
+        help='Contraseña para acceder al presupuesto de este departamento'
     )
 
     @api.model
