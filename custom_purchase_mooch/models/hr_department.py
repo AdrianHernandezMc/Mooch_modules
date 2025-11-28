63e754a1cbfb21600a559a9864ea3c661ae2e1e5
Adrian.Hernandez <soporteTI@mooch-mx.com>
2025-11-28 17:20:58 -0600

    [CHG]security has been modifed to allow validate order

--- custom_purchase_mooch/models/hr_department.py
+++ custom_purchase_mooch/models/hr_department.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class Department(models.Model):
     _inherit = 'hr.department'
@@ -8,7 +8,21 @@ class Department(models.Model):
         string='Cuenta Analítica',
         help='Cuenta analítica vinculada a este departamento',
     )
+    
     budget_password = fields.Char(
         string='Contraseña de Presupuesto',
-        help='Contraseña para acceder al presupuesto de este departamento'
+        help='Contraseña para acceder al presupuesto de este departamento',
     )
+
+    @api.model
+    def fields_get(self, allfields=None, attributes=None):
+        """Sobrescribir para ocultar el campo a usuarios no administradores"""
+        fields = super().fields_get(allfields, attributes)
+        
+        # Si el usuario no es administrador, ocultar budget_password
+        if not self.env.user.has_group('base.group_system'):
+            if 'budget_password' in fields:
+                fields['budget_password']['selectable'] = False
+                fields['budget_password']['searchable'] = False
+        
+        return fields
